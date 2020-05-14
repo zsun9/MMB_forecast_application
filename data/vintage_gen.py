f16bf73c19da73be035f3948884a2d3961da2b65
zsun <42411119+zexisun@users.noreply.github.com>
2020-05-14 12:39:04 +0200

    Code updates

---
+++ data/vintage_gen.py
@@ -0,0 +1,10 @@
+def vintage_gen(vintage_date = '', start_quarter = '', end_quarter = '', raw_variables = [], observed_variables = []):
+
+    import pathlib, warnings
+    import pandas as pd
+    import numpy as np
+    import datetime as datetime
+
+
+if __name__ == '__main__':
+    pass
