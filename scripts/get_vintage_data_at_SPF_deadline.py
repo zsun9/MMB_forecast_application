af4da173eb9cf77ec4b45fc3c7566bf3b3f661a1
zsun <andrewzsun90@gmail.com>
2020-05-26 17:52:47 +0200

    Data updates

--- scripts/get_vintage_data_at_SPF_deadline.py
+++ scripts/get_vintage_data_at_SPF_deadline.py
@@ -1,19 +1,20 @@
 import pathlib
 import pandas as pd
-import get_vintage_data, sample_ends_at_vintagedate
+import get_vintage_data, create_sample_ends_at_vintagedate
 
 infoSpf = pd.read_csv(pathlib.Path('../data')  / 'spf_dates.csv', encoding='utf-8', index_col=0)
 
 for deadline in infoSpf['deadlines']:
 
-    if pd.to_datetime(deadline).year in {2009}: # 2001, 2008, 2020
-        quarterStart, quarterEnd = sample_ends_at_vintagedate.main(vintageDate=deadline, sampleSize=100)
+    if pd.to_datetime(deadline).year in {2001, 2008, 2009, 2020}: # 2001, 2008, 2020
+        quarterStart, quarterEnd = create_sample_ends_at_vintagedate.main(vintageDate=deadline, sampleSize=100)
         get_vintage_data.main(
 
             vintageDate=deadline, quarterStart=str(quarterStart), quarterEnd=str(quarterEnd),
             observed=[
                 'gdp_rgd_obs', 'gdpdef_obs', 'ffr_obs', 'ifi_rgd_obs', 'c_rgd_obs', 
                 'wage_rgd_obs', 'baag10_obs', 'hours_dngs15_obs', 'hours_sw07_obs',
+                'gdpl_rgd_obs',
                 ]
 
             )
