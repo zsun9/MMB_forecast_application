d1a1aedece7993c82a223ae72424f8852ff8acf6
jasperiankl <32761724+jasperiankl@users.noreply.github.com>
2020-07-28 22:33:03 +0200

    IN10 estimation part 1

---
+++ estimations/IN10_20090210_s1/IN10_set_auxiliary_variables.m
@@ -0,0 +1,7 @@
+function y = IN10_set_auxiliary_variables(y, x, params)
+%
+% Status : Computes static model for Dynare
+%
+% Warning : this file is generated automatically by Dynare
+%           from model file (.mod)
+
