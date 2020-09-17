4c3850dc503cbe5719df6b8e28db6ab3ac26fd72
Alexle1709 <45760141+Alexle1709@users.noreply.github.com>
2020-09-18 00:39:38 +0200

    VI16_20

---
+++ estimations/VI16_BGG_20200812_s4/VI16_BGG_set_auxiliary_variables.m
@@ -0,0 +1,7 @@
+function y = VI16_BGG_set_auxiliary_variables(y, x, params)
+%
+% Status : Computes static model for Dynare
+%
+% Warning : this file is generated automatically by Dynare
+%           from model file (.mod)
+
